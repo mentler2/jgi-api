a5727b0066b4e2f015e6f6ba9141ee4e002e03e1
mentler2 <mentler2@gmail.com>
2018-04-27 14:15:54 -0400

    Update invoke.sh
    Added help lines to assist general users with input and troubleshooting, formatted print statements for increased clarity, and generalized file type to download.

--- invoke.sh
+++ invoke.sh
@@ -1,53 +1,64 @@
-#!bash/bin/
+#!bin/bash
 
 username=$(read -p "JGI Username: ")
 password=$(read -s -p "JGI Password: ")
 
+echo ''
+echo ''
 curl 'https://signon-old.jgi.doe.gov/signon/create' --data-urlencode "login=$username" --data-urlencode "password=$password" -c cookies 
-node_name=$(read -p "\nNode name (e.g., 'fungi,' or 'pucciniomycotina'): ")
-curl --cookie cookies --output files.xml "https://genome.jgi.doe.gov/portal/ext-api/downloads/get-directory?organism=$node_name&organizedByFileType=false"
-size_file_heirarchy=$(ls -s files.xml)
+echo ''
+echo ''
+node_name=$(read -p "Node name (e.g., 'fungi,' or 'pucciniomycotina'): ")
+mkdir temp_files
+curl --cookie cookies --output temp_files/files.xml "https://genome.jgi.doe.gov/portal/ext-api/downloads/get-directory?organism=$node_name&organizedByFileType=false"
+size_file_heirarchy=$(ls -s temp_files/files.xml)
 size_file_heirarchy=${size_file_heirarchy:0:1}
-if [ "$size_file_heirarchy" -gt "0" ]; then
+echo ''
+if [ "$size_file_heirarchy" -gt "50" ]; then #if file file is larger than the size required for the default error statement
 	echo 'File heirarchy downloaded as files.xml.'
 else
-	echo 'problem obtaining file heirarchy'
+	echo 'Problem obtaining file heirarchy. Please ensure you are attempting to interrogate a valid node. This can be done by checking the JGI genome database for the presence of a download option. If you are working within a restricted network your shell settings may need to be adjusted to permit downloads.'
 fi
-#grab md5s
-grep -E 'AssemblyScaffolds_Repeatmasked.fasta.gz' "files.xml" | grep -Eo 'md5=".+"' > unformatted_md5s.txt
+echo ''
+
+printf 'Enter search string to interrogate file heirarchy. Suffices are useful for grabbing particular types of files. For example, "AssemblyScaffolds_Repeatmasked.fasta.gz"'
+echo ''
+search_string=$(read -p "search string: ")
+grep -E "$search_string" "temp_files/files.xml" | grep -Eo 'md5=".+"' > temp_files/unformatted_md5s.txt
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "$line" | sed -e 's/md5="//' | sed -e 's/"//' >> md5s.txt
-done < "unformatted_md5s.txt"
-size_md5s=$(ls -s md5s.txt)
+    echo "$line" | sed -e 's/md5="//' | sed -e 's/"//' >> temp_files/md5s.txt
+done < "temp_files/unformatted_md5s.txt"
+size_md5s=$(ls -s temp_files/md5s.txt)
 size_md5s=${size_md5s:0:1}
 if [ "$size_md5s" -gt "0" ]; then
 	echo 'md5s prepared'
 else
-	echo 'problem obtaining md5s'
+	echo 'Problem obtaining md5s.'
 fi
 
 #grab urls based on unique md5sums
-sort md5s.txt | uniq > unique_md5s.txt
+sort temp_files/md5s.txt | uniq > temp_files/unique_md5s.txt
 
 while IFS='' read -r line || [[ -n "$line" ]]; do 
-	grep -m 1 "$line" "files.xml" >> unique_lines.txt
-done < "unique_md5s.txt"
+	grep -m 1 "$line" "files.xml" >> temp_files/unique_lines.txt
+done < "temp_files/unique_md5s.txt"
 
-grep -Eo '/portal/.+AssemblyScaffolds_Repeatmasked.fasta.gz' unique_lines.txt > urls.txt
+grep -Eo '/portal/.+AssemblyScaffolds_Repeatmasked.fasta.gz' temp_files/unique_lines.txt > temp_files/urls.txt
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "https://genome.jgi.doe.gov$line" | sed -e 's/amp;//' >> formatted_urls.txt
-done < "urls.txt"
-echo 'urls grabbed and formatted'
-python make_files.py formatted_urls.txt unique_lines.txt curls.sh unique_md5s.txt md5s_and_filenames.txt
-echo 'md5 and curl commmand files written'
+    echo "https://genome.jgi.doe.gov$line" | sed -e 's/amp;//' >> temp_files/formatted_urls.txt
+done < "temp_files/urls.txt"
+echo 'File urls grabbed and formatted.'
+python make_files.py temp_files/formatted_urls.txt temp_files/unique_lines.txt temp_files/curls.sh temp_files/unique_md5s.txt temp_files/md5s_and_filenames.txt
+printf 'Commmand files written.'
 num_downloads=$(wc -l < "curls.sh")
-echo "$num_downloads files to download"
-echo 'beginning curls execution'
+printf "$num_downloads files awaiting download."
+printf 'Beginning curls execution.'
 bash curls.sh
-echo "sequences of $1 downloaded"
+printf "Curl executions complete."
 
 #check md5
-md5sum * > local_md5s.txt
-sort md5s_and_filenames.txt > md5s_from_database_sorted.txt
-sort local_md5s.txt > local_md5s_sorted.txt
-diff md5s_from_database_sorted.txt local_md5s_sorted.txt
+md5sum * > temp_files/local_md5s.txt
+sort temp_files/md5s_and_filenames.txt > temp_files/md5s_from_database_sorted.txt
+sort temp_files/local_md5s.txt > temp_files/local_md5s_sorted.txt
+diff temp_files/md5s_from_database_sorted.txt temp_files/local_md5s_sorted.txt
