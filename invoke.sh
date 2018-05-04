9d9ff8b75c0351938d81387936a1ea37913f9f3d
mentler2 <mentler2@gmail.com>
2018-05-04 16:04:44 -0400

    Update invoke.sh
    Added exit after failed login attempt, made file hierarchy check more robust, and generalized search so it no longer requires file suffices.

--- invoke.sh
+++ invoke.sh
@@ -3,29 +3,35 @@
 read -p "JGI Username: " username
 read -s -p "JGI Password: " password
 
-echo ''
-echo ''
-curl 'https://signon-old.jgi.doe.gov/signon/create' --data-urlencode "login=$username" --data-urlencode "password=$password" -c cookies 
-echo ''
-echo ''
-read -p "Node name (e.g., 'fungi,' or 'pucciniomycotina'): " node_name
 mkdir temp_files
-curl --cookie cookies --output temp_files/files.xml "https://genome.jgi.doe.gov/portal/ext-api/downloads/get-directory?organism=$node_name&organizedByFileType=false"
-size_file_heirarchy=($(ls -s temp_files/files.xml)) #output goes into an array
 echo ''
-if [ "${size_file_heirarchy[0]}" -gt "50" ]; then #if file file is larger than the size required for the default error statement
+echo ''
+curl -s 'https://signon-old.jgi.doe.gov/signon/create' --data-urlencode "login=$username" --data-urlencode "password=$password" -c temp_files/cookies 
+lines_cookies=($(wc -l temp_files/cookies))
+if [ "${lines_cookies[0]}" -eq "6" ]; then
+	echo ''
+	echo "Successful login."
+	echo ''
+else
+	echo "Unsuccessful login. Terminating script."
+	exit 1
+fi
+read -p "Node name (e.g., 'fungi,' or 'pucciniomycotina'): " node_name
+curl --cookie temp_files/cookies --output temp_files/files.xml "https://genome.jgi.doe.gov/portal/ext-api/downloads/get-directory?organism=$node_name&organizedByFileType=false"
+lines_file_heirarchy=($(wc -l temp_files/files.xml)) #output goes into an array
+echo ''
+if [ "${lines_file_heirarchy[0]}" -gt 2 ]; then #if more lines exist than the number required for default error statements
 	echo 'File heirarchy downloaded to temp_files/files.xml.'
 else
 	echo 'Problem obtaining file heirarchy. Please ensure you are attempting to interrogate a valid node. This can be done by checking the JGI genome database for the presence of a download option. If you are working within a restricted network your shell settings may need to be adjusted to permit downloads.'
 	echo ''
 	rm -r temp_files
-	rm cookies
 	echo 'Script terminated.'
 	exit 1
 fi
 echo ''
 
-echo 'Enter search string to interrogate file heirarchy. Suffix is required if you would like to conserve the file name. For example, "AssemblyScaffolds_Repeatmasked.fasta.gz". If necessary you can peruse the downloaded file heirarchy to get an idea of available file types.'
+echo 'Enter search string to interrogate file heirarchy. For example, "AssemblyScaffolds_Repeatmasked". If necessary you can peruse the downloaded file heirarchy to see available file types.'
 echo ''
 read -p "Search string: " search_string
 grep -E "$search_string" "temp_files/files.xml" | grep -Eo 'md5=".+"' > temp_files/unformatted_md5s.txt
@@ -50,7 +56,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	grep -m 1 "$line" "temp_files/files.xml" >> temp_files/unique_lines.txt
 done < "temp_files/unique_md5s.txt"
 
-grep -Eo '/portal/.+AssemblyScaffolds_Repeatmasked.fasta.gz' temp_files/unique_lines.txt > temp_files/urls.txt
+grep -Eo "url=\".*$search_string[^\"]*\"" temp_files/unique_lines.txt | sed -e 's/url="//' | sed -e 's/^"//' -e 's/"$//' > temp_files/urls.txt
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "https://genome.jgi.doe.gov$line" | sed -e 's/amp;//' >> temp_files/formatted_urls.txt
 done < "temp_files/urls.txt"
@@ -67,14 +73,17 @@ python make_files.py temp_files/formatted_urls.txt temp_files/unique_lines.txt t
 echo 'Commmand files written.'
 num_downloads=$(wc -l < "temp_files/curls.sh")
 echo "$num_downloads files awaiting download."
+echo ''
 echo 'Beginning curls execution.'
 bash temp_files/curls.sh
 echo "Curl executions complete."
 
 #check md5
-md5sum * > temp_files/local_md5s.txt
+md5sum *$search_string* > temp_files/local_md5s.txt
 sort temp_files/md5s_and_filenames.txt > temp_files/md5s_from_database_sorted.txt
 sort temp_files/local_md5s.txt > temp_files/local_md5s_sorted.txt
-diff temp_files/md5s_from_database_sorted.txt temp_files/local_md5s_sorted.txt > temp_files/md5sum_check.txt
-
-echo "Script complete. See temp_files/md5sum_check.txt for to verify files are uncorrupted."
+echo ''
+echo "Calculating md5sums..."
+diff -y temp_files/md5s_from_database_sorted.txt temp_files/local_md5s_sorted.txt
+echo ''
+echo "Script complete."
